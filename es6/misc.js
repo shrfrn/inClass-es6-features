f5f5f543e7d7a8400fb5a39c3c7fd52613f7c8d0
shrfrn <shrfrn@gmail.com>
2023-06-13 16:40:33 +0300

    Some changes to Array.from() demo

--- es6/misc.js
+++ es6/misc.js
@@ -7,9 +7,16 @@ function createPerson(fullName='Muki', score=parseInt(Math.random()*100), val=fu
         val
     }
 }
-console.log('Person', createPerson())
+// console.log('Person', createPerson())
 
 // Returns a real Array:
 
 Array.from(document.querySelectorAll('*')) 
-console.log('Also:', [...document.querySelectorAll('*')])
+// console.log('Also:', [...document.querySelectorAll('*')])
+
+// func(1, 2, 3)
+
+function func() {
+    console.log(Array.from(arguments));
+    // console.log([...arguments]);
+}
