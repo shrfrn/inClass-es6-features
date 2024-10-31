194ce4b9d87e3e67aaa06cc9218664370ffeab86
shrfrn <shrfrn@gmail.com>
2024-10-31 15:25:53 +0200

    Add intro to local storage

--- es6/local-storage.js
+++ es6/local-storage.js
@@ -1,5 +1,45 @@
 'use strict'
 
+// 01 - Basics
+
+localStorage.setItem('numOfApples', '3')
+const numOfApples = localStorage.getItem('numOfApples')
+
+console.log(numOfApples)
+
+// 02 - Stringify arrays & objects
+
+const pet = { 
+    name: 'Bobbi', 
+    age: 3, 
+    nicknames: ['bob', 'doodoo'] 
+}
+
+// This will not work...
+
+// localStorage.setItem('pet', pet)
+// const petFromStorage = localStorage.getItem('pet')
+
+// ...we need to stringify & parse
+
+// var json = JSON.stringify(pet)
+// localStorage.setItem('pet', json)
+
+// jsonFromStorage = localStorage.getItem('pet')
+// const petFromStorage = JSON.parse(jsonFromStorage)
+
+// const students = [
+//     { name: 'Arik', age: 10 },
+//     { name: 'Palik', age: 10 },
+//     { name: 'Malik', age: 10 },
+// ]
+
+// var json = JSON.stringify(students)
+// localStorage.setItem('student', json)
+
+// jsonFromStorage = localStorage.getItem('student')
+// const StudentsFromStorage = JSON.parse(jsonFromStorage)
+
 // Local Storage - Functional interface vs. dot notation
 // When the key we are trying to retrieve doesn't exist...
 
